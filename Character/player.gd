05e166911b2694aa2be9b6bbf4ba3355cc053a99
KamielFlamiel <justinmeijer2007@outlook.com>
2025-02-05 14:00:00 +0100

    wah

--- Character/player.gd
+++ Character/player.gd
@@ -1,27 +1,32 @@
 extends CharacterBody2D
 
 
-@export var SPEED : float = 200.0
+@export var NORMALSPEED : float = 200.0
 @export var JUMP_VELOCITY : float = -250.0
-
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var animation_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
 
+const dashspeed = 800
+const dashlength = .1
+
+@onready var dash = $Dash
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	if Input.is_action_just_pressed("dash"):
+		dash.start_dash(dashlength)
+	var SPEED = dashspeed if dash.is_dashing() else NORMALSPEED
+
 	direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
 		velocity.x = direction.x * SPEED
