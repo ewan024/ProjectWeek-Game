f302e27bbad786c29b1d3b1c08e725fb8b990227
ewan024 <1212081@student.roc-nijmegen.nl>
2025-02-10 12:00:27 +0100

    ynb adfsop

--- Signal_Bus.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-# Global singleton to connect signal callers to receivers
-# through a single object
-#
-# Helpful when a receiver needs to receive all signal calls
-# without connecting directly to every caller in the scene
-
-signal on_health_changed(node : Node, amount_changed : int)
